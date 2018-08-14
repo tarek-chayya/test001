dc2b8a3bfce9db348cb18837dfc7fdfbbe2b2024
Tarek <tarek.chayya@ethoskms.com>
2018-08-14 16:11:40 +0300

    Fix Indent Signed-off-by: Tarek <tarek.chayya@ethoskms.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,15 @@
 
 var wwidth, dwidth;
 $(function() {
-  swidth=$("#swidth");
-  DisplayScreenWidth();
-  $( window ).resize(function() {
-	  DisplayScreenWidth();
-  });
-  function DisplayScreenWidth(){
-	  varsw=$( window ).width();
-	  wwidth.html(varsw+"px / ");
-	  varsw=$( document ).width();
-	  dwidth.append(varsw+"px ");
-  }
+	swidth=$("#swidth");
+	DisplayScreenWidth();
+	$( window ).resize(function() {
+		DisplayScreenWidth();
+	});
 });
+function DisplayScreenWidth(){
+	varsw=$( window ).width();
+	wwidth.html(varsw+"px / ");
+	varsw=$( document ).width();
+	dwidth.append(varsw+"px ");
+}
