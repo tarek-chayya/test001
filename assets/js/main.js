d9285680e5effe440cca9638c6223a580d2124f3
Tarek <tarek.chayya@ethoskms.com>
2018-08-14 16:32:35 +0300

    Add Time functionality Signed-off-by: Tarek <tarek.chayya@ethoskms.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
 // JavaScript Document
 
-var wwidth, dwidth;
+var wwidth, dwidth, itsnow;
+var dt = new Date();
+var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 $(function() {
 	wwidth=$("#wwidth");
 	dwidth=$("#dwidth");
+	itsnow=$("#itsnow");
 	DisplayScreenWidth();
 	$( window ).resize(function() {
 		DisplayScreenWidth();
 	});
+	itsnow.html(time);
 	function DisplayScreenWidth(){
 		console.log("Set Screen");
 		varsw=$( window ).width();
