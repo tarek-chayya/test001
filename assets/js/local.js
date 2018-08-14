3c4266faa12ab48e90ef862026dae40289b98801
Tarek <tarek.chayya@ethoskms.com>
2018-08-14 16:20:19 +0300

    add local

---
+++ assets/js/local.js
@@ -0,0 +1,2 @@
+// JavaScript Document
+console.log("This is the local loaded");
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,17 @@
 
 var wwidth, dwidth;
 $(function() {
-	swidth=$("#swidth");
+	wwidth=$("#wwidth");
+	dwidth=$("#dwidth");
 	DisplayScreenWidth();
 	$( window ).resize(function() {
 		DisplayScreenWidth();
 	});
+	function DisplayScreenWidth(){
+		console.log("Set Screen");
+		varsw=$( window ).width();
+		wwidth.html(varsw+"px");
+		varsw=$( document ).width();
+		dwidth.append(varsw+"px ");
+	}
 });
-function DisplayScreenWidth(){
-	varsw=$( window ).width();
-	wwidth.html(varsw+"px / ");
-	varsw=$( document ).width();
-	dwidth.append(varsw+"px ");
-}
