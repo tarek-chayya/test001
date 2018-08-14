1cd6ab37b80c25627ee0155f15456fd2d57b54b7
Tarek <tarek.chayya@ethoskms.com>
2018-08-14 16:06:54 +0300

    Signed-off-by: Tarek <tarek.chayya@ethoskms.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
 // JavaScript Document
 
+var swidth;
 $(function() {
   // Handler for .ready() called.
-  
+  swidth=$("#swidth");
+  DisplayScreenWidth();
+  $( window ).resize(function() {
+	  DisplayScreenWidth();
+  });
+  function DisplayScreenWidth(){
+	  varsw=$( window ).width();
+	  swidth.html(varsw+"px / ");
+	  varsw=$( document ).width();
+	  swidth.append(varsw+"px ");
+  }
 });
